163a410fc34f70484ea860644485319bb271463e
Huaiyuan Jing <ls.hyjing@gmail.com>
2023-12-01 22:38:52 -0600

    initialize

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInputControl inputControl;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    public Vector2 inputDirection;
+    public float speed;
+
+    private void Awake()
+    {
+        inputControl = new PlayerInputControl();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+    private void Update()
+    {
+        inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+
+        // Flip character
+        spriteRenderer.flipX = inputDirection.x <= 0 && (inputDirection.x < 0 || spriteRenderer.flipX);
+    }
+}
+
