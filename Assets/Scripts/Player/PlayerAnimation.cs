d730252b335684d670405c982db3ba32cef58a48
Huaiyuan Jing <ls.hyjing@gmail.com>
2023-12-06 20:37:00 -0600

    Reset and Update Jump Anime

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -8,14 +8,18 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
+    private PhysicsCheck physicsCheck;
     private void Awake() {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        physicsCheck = GetComponent<PhysicsCheck>();
     }
     private void Update() {
         SetAnimation();
     }
     public void SetAnimation() {
         anim.SetFloat("velocityX", rb.velocity.x > 0 ? rb.velocity.x : -rb.velocity.x);
+        anim.SetFloat("velocityY", rb.velocity.y > 0 ? rb.velocity.y : -rb.velocity.y);
+        anim.SetBool("isGround", physicsCheck.isGround);
     }
 }
