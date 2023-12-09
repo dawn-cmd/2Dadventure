47c59564c6d7ea9637797afa0034de4ec8908e29
Huaiyuan Jing <ls.hyjing@gmail.com>
2023-12-08 20:36:00 -0600

    Update Crouch

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -9,10 +9,12 @@ public class PlayerAnimation : MonoBehaviour
     private Animator anim;
     private Rigidbody2D rb;
     private PhysicsCheck physicsCheck;
+    private PlayerController playerController;
     private void Awake() {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         physicsCheck = GetComponent<PhysicsCheck>();
+        playerController = GetComponent<PlayerController>();
     }
     private void Update() {
         SetAnimation();
@@ -21,5 +23,6 @@ public class PlayerAnimation : MonoBehaviour
         anim.SetFloat("velocityX", rb.velocity.x > 0 ? rb.velocity.x : -rb.velocity.x);
         anim.SetFloat("velocityY", rb.velocity.y > 0 ? rb.velocity.y : -rb.velocity.y);
         anim.SetBool("isGround", physicsCheck.isGround);
+        anim.SetBool("isCrouch", playerController.isCrouch);
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// Controls the movement and behavior of the player character.
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
     public PlayerInputControl inputControl;
@@ -11,12 +14,16 @@ public class PlayerController : MonoBehaviour
     private PhysicsCheck physicsCheck;
     private SpriteRenderer spriteRenderer;
     public Vector2 inputDirection;
+    private CapsuleCollider2D coll;
+    private Vector2 originalOffset;
+    private Vector2 originalSize;
     [Header("Basic Parameter")]
     public float speed;
     private float walkSpeed => speed / 2.5f;
     private float runSpeed;
     public float jumpForce;
     public bool enableDoubleJump;
+    public bool isCrouch;
 
     private void Awake()
     {
@@ -30,6 +37,9 @@ public class PlayerController : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
 
         physicsCheck = GetComponent<PhysicsCheck>();
+        coll = GetComponent<CapsuleCollider2D>();
+        originalOffset = coll.offset;
+        originalSize = coll.size;
         #region ForceWalk
         runSpeed = speed;
         inputControl.Gameplay.WalkButton.performed += ctx =>
@@ -41,6 +51,7 @@ public class PlayerController : MonoBehaviour
             if (physicsCheck.isGround) speed = runSpeed;
         };
         #endregion  
+
     }
 
 
@@ -64,11 +75,26 @@ public class PlayerController : MonoBehaviour
     }
 
     public void Move()
-    {
-        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+    { 
+        rb.velocity = new Vector2(isCrouch ? 0 : inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
 
         // Flip character
         spriteRenderer.flipX = inputDirection.x <= 0 && (inputDirection.x < 0 || spriteRenderer.flipX);
+
+        // Crouch
+        isCrouch = inputDirection.y < -0.5f && physicsCheck.isGround;
+        if (isCrouch == true)
+        {
+            // change block
+            coll.offset = new Vector2(-0.05f, 0.85f);
+            coll.size = new Vector2(0.66f, 1.6f);
+        }
+        else
+        {
+            // reset block
+            coll.offset = originalOffset;
+            coll.size = originalSize;
+        }
     }
     private void Jump(InputAction.CallbackContext context)
     {
